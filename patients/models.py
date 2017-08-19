ede83d9b4fac7b9a92dbe7c2df8641631112db92
siham09 <sihamsayeed9@gmail.com>
2017-08-19 17:13:16 +0600

    model update

--- patients/models.py
+++ patients/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 class Temp_Data(models.Model):
     name = models.CharField(max_length=100)
-    initial = models.FloatField()
-    latest = models.FloatField()
-    times = models.IntegerField()
+    temperature = models.FloatField()
+#    latest = models.FloatField()
+#    times = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class Pulse_Data(models.Model):
     name = models.CharField(max_length=100)
-    initial = models.FloatField()
-    latest = models.FloatField()
-    times = models.IntegerField()
+#    initial = models.FloatField()
+    hRate = models.FloatField()
+#    times = models.IntegerField()
 
     def __str__(self):
         return self.name
